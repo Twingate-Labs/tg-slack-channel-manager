d67378950d1d6997eb4ea299dd83e0ccc7b96d64
chenbishop <chen.bishop@gmail.com>
2022-10-05 17:57:28 +0100

    cloudrun testing

--- app.js
+++ app.js
@@ -6,7 +6,16 @@ const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
 
 
 
-function initApp(app){
+async function initApp(app){
+    let mergedChannelId = ""
+    let logString = ""
+
+    const channels = await app.client.conversations.list()
+    const extAllChannel = channels.channels.filter(channel => channel.name === "ext-all")[0]
+    if (extAllChannel !== undefined){
+        mergedChannelId = extAllChannel.id
+        console.log(`[INFO] Setting ext-all channel ID: ${mergedChannelId}`)
+    }
 
     app.message('', async ({ message, client, say,logger }) => {
         const userResult = await client.users.info({user: message.user})
@@ -17,45 +26,43 @@ function initApp(app){
             const result = await client.chat.postMessage({
                 channel: mergedChannelId,
                 text: `${userResult.user.real_name} from <${chatResult.permalink}|${channelResult.channel.name}>`
-                // text: `${userResult.user.real_name} from <${chatResult.permalink}|${channelResult.channel.name}> said:\n${message.text}`
             });
             logString = `new msg added ${userResult.user.real_name} from <${chatResult.permalink}|${channelResult.channel.name}> said: ${message.text}`
             logger.info(logString)
-            // await client.chat.postMessage({channel: logChannelId, text: `[INFO]  ${logString}`})
         } else if (channelResult.channel.name.startsWith("ext-all")){
             logString = `Channel name ${channelResult.channel.name} starts with ext-all, message Ignored`
             logger.info(logString)
-            // await client.chat.postMessage({channel: logChannelId, text: `[INFO]  ${logString}`})
         } else {
             logString = `Channel name ${channelResult.channel.name} does not starts with ext-, message Ignored`
             logger.info(logString)
-            // await client.chat.postMessage({channel: logChannelId, text: `[INFO]  ${logString}`})
         }
     });
 
-    app.command("/twingate_create_ext_all_channel", async ({ command, ack, client,say ,logger}) => {
+    app.command("/twingate_channel_merge_setup", async ({ command, ack, client,say ,logger}) => {
         try {
             await ack();
             const channelCreateResult = await client.conversations.create({name: "ext-all"})
             mergedChannelId = channelCreateResult.channel.id
             logString = `channel ext-all has been created with id ${channelCreateResult.channel.id}`
             logger.info(logString)
-            // await client.chat.postMessage({channel: logChannelId, text: `[INFO]  ${logString}`})
             const addUserResult = await client.conversations.invite({channel: channelCreateResult.channel.id, users: command.user_id})
             logString = `added command issue user ${command.user_name} to ext-all channel`
             logger.info(logString)
-            // await client.chat.postMessage({channel: logChannelId, text: `[INFO]  ${logString}`})
-            await updateDotenv({
-                MERGED_CHANNEL_ID: channelCreateResult.channel.id
-            })
+
+            const allChannelsResult = await client.conversations.list({limit: 1000})
+            const extChannels = allChannelsResult.channels.filter(x => x.name.startsWith("ext-") && !x.name.startsWith("ext-all"))
+            let channelJoinResult = ""
+            for (const channel of extChannels) {
+                channelJoinResult = await client.conversations.join({channel: channel.id})
+                logString = `Bot joined channel ${channel.id}`
+                logger.info(logString)
+            }
         } catch (error) {
             logger.error(error);
-            // await client.chat.postMessage({channel: logChannelId, text: `[ERROR]  ${error}`})
         }
+
+
     });
-
-
-
 
     app.command("/twingate_add_all_ext_channel", async ({ command, ack, client,say ,logger}) => {
         try {
@@ -67,40 +74,30 @@ function initApp(app){
                 channelJoinResult = await client.conversations.join({channel: channel.id})
                 logString = `Bot joined channel ${channel.id}`
                 logger.info(logString)
-                // await client.chat.postMessage({channel: logChannelId, text: `[INFO]  ${logString}`})
             }
         } catch (error) {
             logger.error(error);
-            // await client.chat.postMessage({channel: logChannelId, text: `[ERROR]  ${error}`})
         }
     });
 
     app.event('channel_created', async ({ event, client, logger }) => {
         try {
-            // Call chat.postMessage with the built-in client
             if (event.channel.name.startsWith("ext-") && !event.channel.name.startsWith("ext-all")){
                 const joinChannelResult = await client.conversations.join({channel: event.channel.id})
                 logString = `Bot joined channel ${event.channel.name}`
                 logger.info(logString)
-                // await client.chat.postMessage({channel: logChannelId, text: `[INFO]  ${logString}`})
             } else {
                 logString = `channel ${event.channel.name} is not an ext- channel, skip`
                 logger.info(logString)
-                // await client.chat.postMessage({channel: logChannelId, text: `[INFO]  ${logString}`})
             }
         }
         catch (error) {
             logger.error(error);
-            // await client.chat.postMessage({channel: logChannelId, text: `[ERROR]  ${error}`})
         }
     });
 
 }
 
-
-let mergedChannelId = process.env.MERGED_CHANNEL_ID
-// let logChannelId = process.env.LOGS_CHANNEL_ID
-let logString = ""
 
 
 async function accessSecretVersion (name) {
@@ -109,24 +106,17 @@ async function accessSecretVersion (name) {
     const [version] = await client.accessSecretVersion({
         name: `projects/${projectId}/secrets/${name}/versions/1`
     })
-
-    // Extract the payload as a string.
     const payload = version.payload.data.toString('utf8')
-
     return payload
 }
 
 (async () => {
     const port = 8080
-    // Initializes your app with your bot token and signing secret
     const app = new App({
         token: await accessSecretVersion('bot-token'),
         signingSecret: await accessSecretVersion('client-signing-secret')
-        // socketMode:true, // enable the following to use socket mode
-        // appToken: process.env.APP_TOKEN
     });
-    // Start your app
-    initApp(app);
+    await initApp(app);
     await app.start(process.env.PORT || port);
     console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
 })();
